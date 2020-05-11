5b33bbe2c223af0e3b0a506a46bb3de274877481
Steffi LaZerte <steffi@steffi.ca>
2020-05-11 10:13:02 -0500

    Switch to single sheet style

--- usecases.R
+++ usecases.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(lubridate)
 library(purrr)
 library(stringr)
+library(glue)
 
 #' Need some dev packages:
 # remotes::install_github("tidyverse/googlesheets4/")
@@ -18,14 +19,17 @@ sheets <- sheet_names(ss)
 # Remove starting Sheet1
 if(any(sheets == "Sheet1")) sheet_delete(ss, "Sheet1")
 
-# Get dates from previous sheets or from 7 days ago
+# Get dates/tweets from previous sheets or from 7 days ago
 if(any(sheets != "Sheet1")) {
-  prev_date <- read_sheet(ss, as.character(max(as_date(sheets))))
+  prev_tweets <- read_sheet(ss, as.character(max(as_datetime(sheets)))) %>%
+    mutate(date_time = as.POSIXct(round(date_time, units = "secs")))
+  prev_date <- max(c(prev_tweets$date_time, as_datetime(sheets)), na.rm = TRUE)
 } else {
-  prev_date <- Sys.Date() - days(7)
+  prev_tweets <- tibble()
+  prev_date <- Sys.time() - days(7)
 }
 
-prev_date <- max(as_date(sheet_names(ss)))
+
 
 #' Twitter users to ignore
 ignore_users <- c("CRANberriesFeed", "ropensci", "tidyversetweets",
@@ -48,6 +52,8 @@ pkgs <- "https://ropensci.github.io/roregistry/registry.json" %>%
 #'   Seem to run into rate limits if try to put package names directly in search
 #' - Consider adding 'lang = "en"' to `search_tweets()` (but this isnt' perfect)
 
+check_time <- trunc(with_tz(Sys.time(), tzone = "UTC"), units = "hours")
+
 tweets_raw <- search_tweets(q = "#rstats OR ropensci OR CRAN",
                             n = 18000,
                             include_rts = FALSE)
@@ -57,60 +63,69 @@ nrow(tweets_raw)
 
 #' Narrow in on what we care about
 tweets_relevant <- tweets_raw %>%
-  select(created_at, screen_name, text, status_url,
-         hashtags, urls_expanded_url) %>%
-  mutate(name = str_extract_all(text,
-                                paste0("\\b", paste0(pkgs$name,
-                                                     collapse = "\\b|\\b"), "\\b")),
-         name = map(name, unique),
-         n = map_int(name, length)) %>%
+  select(date_time = created_at,
+         twitter_user = screen_name,
+         text, status_url,
+         urls_expanded_url) %>%
+  mutate(pkgs = str_extract_all(text,
+                                glue("\\b",
+                                     glue_collapse(pkgs$name, "\\b|\\b"),
+                                     "\\b")),
+         pkgs = map(pkgs, unique),
+         n = map_int(pkgs, length)) %>%
   filter(n > 0)
 
 
 #' Get what we need
 tweets_subset <- tweets_relevant %>%
-  unnest(cols = c(name)) %>%
-  filter(created_at >= prev_date,       # Must be on or after start date
-         !tolower(screen_name) %in% tolower(ignore_users)) %>% # Must not contain ignored users
-  left_join(select(pkgs, name, maintainer), by = "name")
+  unnest(cols = c(pkgs)) %>%
+  filter(date_time >= prev_date,       # Must be on or after start date/time
+         !tolower(twitter_user) %in% tolower(ignore_users)) %>% # Must not contain ignored users
+  left_join(select(pkgs, name, maintainer), by = c("pkgs" = "name"))
 
 #' # Format tweets
 tweets_final <- tweets_subset %>%
   mutate(ropensci_author = stringi::stri_trans_general(maintainer, "Latin-ASCII"),
-         ropensci_author = paste0("https://ropensci.org/author/",
-                                  str_replace_all(ropensci_author,
-                                                  c(" " = "-", "\\." = ""))),
-         ropensci_author = paste0("=HYPERLINK(\"", ropensci_author, "\", \"",
-                                  maintainer, "\")"),
+         ropensci_author = str_replace_all(ropensci_author, c(" " = "-", "\\." = "")),
+         ropensci_author = glue("https://ropensci.org/author/{ropensci_author}"),
+         ropensci_author = glue("=HYPERLINK(\"{ropensci_author}\", \"",
+                                "{maintainer}\")"),
          text = str_wrap(text, width = 40),
-         original_tweet = paste0("=HYPERLINK(\"", status_url,
-                                 "\", \"Original tweet\")")) %>%
-  group_by(created_at, screen_name, text, original_tweet, status_url) %>%
-  summarize(names = paste0(name, collapse = "\n"),
-            date = as_date(created_at[1]),
-            twitter_user = paste0("=HYPERLINK(\"https://twitter.com/",
-                                  screen_name[1], "\", \"",
-                                  screen_name[1], "\")"),
+         original_tweet = glue("=HYPERLINK(\"{status_url}",
+                               "\", \"Original tweet\")")) %>%
+  group_by(date_time, text, original_tweet, status_url) %>%
+  summarize(pkgs = as.character(glue_collapse(pkgs, "\n")),
             ropensci_author = if_else(length(unique(ropensci_author)) > 1,
-                                      paste0("=\"", paste0(maintainer, collapse = " and "), "\""),
+                                      glue("=\"", glue_collapse(maintainer, " and "), "\""),
                                       ropensci_author[1]),
-            draft_tweet = paste0("[use case] Example of using ", names, ", by @", screen_name[1], "\n\n",
-                                 "FORUM_URL\n\n",
-                                 "with CODE and GIF EXAMPLES\n\n",
-                                 names, " by @GET_TWITTER_USER\n\n",
-                                 "#rstats #", paste0(name, collapse = " #"), "\n\n",
-                                 "[GIF? MEDIA?]")) %>%
+            draft_tweet = glue("[use case] Example of using {pkgs}, by @{twitter_user[1]}\n\n",
+                               "FORUM_URL\n\n",
+                               "with CODE and GIF EXAMPLES\n\n",
+                               "{pkgs} by @GET_TWITTER_USER\n\n",
+                               "#rstats #", glue_collapse(pkgs, " #"), "\n\n",
+                               "[GIF? MEDIA? CHECK USER, CHECK HASHTAGS]"),
+            twitter_user = glue("=HYPERLINK(\"https://twitter.com/",
+                                "{twitter_user[1]}\", \"",
+                                "{twitter_user[1]}\")")) %>%
   ungroup() %>%
   mutate_at(vars(ropensci_author, twitter_user, original_tweet),
            gs4_formula) %>%
-  arrange(names, created_at) %>%
+  arrange(pkgs, date_time) %>%
   mutate(`use?` = "", media = "") %>%
-  select(`use?`, names, date, twitter_user, text, original_tweet,
+  select(`use?`, pkgs, date_time, twitter_user, text, original_tweet,
          ropensci_author, draft_tweet, media)
 
 
-sheet_write(tweets_final, ss = ss, sheet = as.character(Sys.Date()))
-
-
-
+# Remove dates already in previous sheets
+if(nrow(prev_tweets) > 0) {
+  tweets_final <- anti_join(tweets_final, prev_tweets,
+                            by = c("pkgs", "date_time"))
+  if(nrow(tweets_final) > 0) {
+    sheet_rename(ss = ss, sheet = sheets, new_name = as.character(check_time))
+    sheet_append(tweets_final, ss = ss, sheet = as.character(check_time))
+  } else message("No new features")
+} else {
+  # If no previous features, start a new sheet
+  sheet_write(tweets_final, ss = ss, sheet = as.character(check_time))
+}
 
